e0bb19e037c9856d5f015a5c9aa2011aa1d07bce
Glenncroles <43144720+Glenncroles@users.noreply.github.com>
2020-03-03 15:56:50 -0600

    A few changes makes it better

--- FinalCalc/src/FinalCalc.cpp
+++ FinalCalc/src/FinalCalc.cpp
@@ -10,6 +10,7 @@
 #include <iomanip>
 using namespace std;
 
+
 int main() {
 	//weighted variable
 	double weighted;
@@ -29,14 +30,41 @@ int main() {
 	cout << "What is the weight of the final?" << endl;
 	cin >> weighted;
 
-	//
 	// ( required grade - (100% - w)×current grade ) / w
 	double nw = weighted/100;
 	double final = ((wanted - ((1-nw) * current))/nw);
 
 	cout << setprecision(2) << fixed;
 
-	cout << "You need a " << final << " to get a " << wanted << ".";
+	cout << "You need a " << final << " to get a " << wanted << ".\n";
+
+	int answer;
+	cout << "Would you like to try another class? (1 for yes or 0 for no)\n";
+	cin >> answer;
+
+	while(answer == 1)
+	{
+			cout << "What is your current grade?" << endl;
+			cin >> current;
+
+			cout << "What is the grade you want?" << endl;
+			cin >> wanted;
+
+			cout << "What is the weight of the final?" << endl;
+			cin >> weighted;
+
+			// ( required grade - (100% - w)×current grade ) / w
+			nw = weighted/100;
+			final = ((wanted - ((1-nw) * current))/nw);
+
+			cout << setprecision(2) << fixed;
+
+			cout << "You need a " << final << " to get a " << wanted << ".\n";
+
+
+			cout << "Would you like to try another class? (1 for yes or 0 for no)\n";
+			cin >> answer;
+	}
 
 	return 0;
 }
